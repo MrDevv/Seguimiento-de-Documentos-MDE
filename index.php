ad828b79f6fc711d73782f84c35bb04ce227024c
Miguel Vega Perez <aries_242015@hotmail.com>
2024-06-17 09:29:02 -0500

    init

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Outfit:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Sistema de Seguimiento de Documentos Internos y Externos</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="assets/logo.png" alt="logo">
+            </div>
+            <div class="menu">
+                <div class="option">
+                    <svg width="31" height="29" viewBox="0 0 31 29" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <rect width="31" height="29" fill="url(#pattern0_2339_10)" />
+                        <defs>
+                            <pattern id="pattern0_2339_10" patternContentUnits="objectBoundingBox" width="1" height="1">
+                                <use xlink:href="#image0_2339_10" transform="matrix(0.00974462 0 0 0.0104167 0.0322581 0)" />
+                            </pattern>
+                            <image id="image0_2339_10" width="96" height="96" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAAACXBIWXMAAAsTAAALEwEAmpwYAAACYklEQVR4nO2cwU7bQABE9wSqgO/tkfxSTz32kPa7HorKttQSqZ3MemfX8yQkpCR4Zl5CgmRTSgghhBBCCGELwMPla9ODggbgEfgGfAe+ZNcdAZ6BH/zl8v1zJOwz/tNi/Mov4CUS+oxfiYSO41cioeP4lUjoOH4lEjqOX4mEOz9qnrnOeeV98hG1wTP/5+Vj58r75pXQYvyy7TGR0GL8SiR0HL8SCR3Hr0RCx/ErkdBx/EokdBy/Egkdx69EQsfxK5HQcfzK4SX0HL8cXYLD+IeV4DT+4SQ4jn8YCc7jTy9hhPGnlTDS+NNJGHH8aSSMPP7wEmYYf1gJM40/nIQZxx9Gwszj20s4wvi2Eo40vp2ElacLMuOpgPzuTrfTILecKFsmhXXoXwlbz1Iuk8J6dBJuOUW8TArbuF/Crefnl0lhO7dLeL8Ies0brlQAjemQ7XzzxeTA6T8/2KXkahplu/ZEPZU7D/r1ysURe5aU0CjbZ7+qX+853mcS/vyRtXNJCS2yvd+2lKAZfyFheWXKbiVVtMj24fYqQTv+hwP882ayZ0kVLbIt7rPff2/pVXL0bDKcS2KcTYZzSYyzyXAuiXE2Gc4lMc4mw7kkxtlkOJfEOJsM55IYZ5PhXBLjbDKcS2KcTYZzSYyzyXAuiXE2Gc4lMc4mw7kkxtlkOJfEOJsM55IYZ5PhXBLjbDKcS2KcTYZzSYyzyXAuiXE2Gc4lMc4mw7kkxtlkOJfEOJsM55IYZ5PBjpSJsslwLolxNhnOJTHOJsO5JMbZZDiXxDibDOeSGGcLIYQQQgghhBBCCOWQvAEyTzle7v8enQAAAABJRU5ErkJggg==" />
+                        </defs>
+                    </svg>
+                    <p>Inicio</p>
+                </div>
+                <div class="option">
+                    <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M9 10C10.364 10 11.6721 9.47322 12.6365 8.53553C13.601 7.59785 14.1429 6.32608 14.1429 5C14.1429 3.67392 13.601 2.40215 12.6365 1.46447C11.6721 0.526784 10.364 0 9 0C7.63603 0 6.32792 0.526784 5.36345 1.46447C4.39898 2.40215 3.85714 3.67392 3.85714 5C3.85714 6.32608 4.39898 7.59785 5.36345 8.53553C6.32792 9.47322 7.63603 10 9 10ZM7.16384 11.875C3.20625 11.875 0 14.9922 0 18.8398C0 19.4805 0.534375 20 1.1933 20H16.8067C17.4656 20 18 19.4805 18 18.8398C18 14.9922 14.7938 11.875 10.8362 11.875H7.16384Z" fill="white" />
+                    </svg>
+                    <div>
+                        <p>Administrados</p>
+                        <svg width="9" height="10" viewBox="0 0 9 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.37924 6.21216L0.635203 9.86453L0.619224 7.84755L6.12301 5.43456L6.06136 5.543L6.05929 5.28165L6.12265 5.3891L0.581324 3.06361L0.565345 1.04663L8.36628 4.57585L8.37924 6.21216Z" fill="white" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main">
+            <h1>main</h1>
+        </div>
+    </div>
+</body>
+
+</html>
--- scrips.sql
+++ scrips.sql
@@ -17,10 +17,18 @@ values('Andrea', 'Vasquez Rojas', '912001111', 1, 2, 3)
 insert into Documento(NroDocumento, asunto, folios, codEstado, codTipoDocumento, fechaRegistro)
 values('9844', 'Deducción', 1, 3, 1, '30-05-2024')
 
+----- movimiento 1
 -- Envio
 insert into Envio(fechaEnvio, codAdministrado, codEstado)
 values('30-05-2024', 1, 5, '9844')
+-- Recepcion
+insert into Recepcion(fechaRecepcion, codAdministrado, codEstado)
+values('01-06-2024', 2, 1, '9844')
 
+----- movimiento 2
+-- Envio
+insert into Envio(fechaEnvio, codAdministrado, codEstado)
+values('02-06-2024', 2, 5, '9844')
 -- Recepcion
 insert into Recepcion(fechaRecepcion, codAdministrado, codEstado)
 values('01-06-2024', 2, 1, '9844')
@@ -39,12 +47,12 @@ select * from Envio
 select * from Recepcion;
 select * from movimiento;
 
+update Recepcion set fechaRecepcion = NULL where codRecepcion = 1
 
-
--- consulta seguimiento
-select d.NroDocumento, d.asunto, d.folios, tp.descripcion as 'Tipo Documento',
-e.fechaEnvio as 'Fecha Derivacion', a.nombres as 'Administrador Origen', 
-r.fechaRecepcion as 'Fecha Recepcion', ar.nombres as 'Administrado Recepcion'
+-- consulta detalle movimiento de un documento
+select d.NroDocumento 'NUMERO DE DOCUMENTO', d.folios 'FOLIOS', d.asunto 'ASUNTO', tp.descripcion 'TIPO DOCUMENTO',
+aro.descripcion 'AREA ORIGEN', a.nombres 'ADMINISTRADO ORIGEN', e.fechaEnvio 'FECHA DERIVACION',  e.observacion 'OBSERVACION', ee.descripcion 'ESTADO ENVIO',
+ard.descripcion 'AREA DESTINO', ar.nombres 'ADMINISTRADO RECEPCION', r.fechaRecepcion 'FECHA RECEPCION', er.descripcion 'ESTADO RECEPCION'
 from Movimiento as m
 inner join Documento as d on m.NroDocumento = d.NroDocumento
 inner join TipoDocumento as tp on d.codTipoDocumento = tp.codTipoDocumento
@@ -52,5 +60,25 @@ inner join Envio as e on m.codEnvio = e.codEnvio
 inner join Recepcion as r on m.codRecepcion = r.codRecepcion
 inner join Administrado as a on e.codAdministrado = a.codAdministrado
 inner join Administrado as ar on r.codAdministrado = ar.codAdministrado
+inner join Estado as ee on ee.codEstado = e.codEstado
+inner join Estado er on er.codEstado = r.codEstado
+inner join Area aro on aro.codArea = a.codArea
+inner join Area ard on ard.codArea = ar.codArea
+
+-- cosulta ver seguimiento de un documento
+select d.NroDocumento 'NUMERO DE DOCUMENTO', d.folios 'FOLIOS', d.asunto 'ASUNTO', tp.descripcion 'TIPO DOCUMENTO',
+aro.descripcion 'AREA ORIGEN', a.nombres 'ADMINISTRADO ORIGEN', e.fechaEnvio 'FECHA DERIVACION',  e.observacion 'OBSERVACION', ee.descripcion 'ESTADO ENVIO',
+ard.descripcion 'AREA DESTINO', ar.nombres 'ADMINISTRADO RECEPCION', r.fechaRecepcion 'FECHA RECEPCION', er.descripcion 'ESTADO RECEPCION'
+from Movimiento as m
+inner join Documento as d on m.NroDocumento = d.NroDocumento
+inner join TipoDocumento as tp on d.codTipoDocumento = tp.codTipoDocumento
+inner join Envio as e on m.codEnvio = e.codEnvio
+inner join Recepcion as r on m.codRecepcion = r.codRecepcion
+inner join Administrado as a on e.codAdministrado = a.codAdministrado
+inner join Administrado as ar on r.codAdministrado = ar.codAdministrado
+inner join Estado as ee on ee.codEstado = e.codEstado
+inner join Estado er on er.codEstado = r.codEstado
+inner join Area aro on aro.codArea = a.codArea
+inner join Area ard on ard.codArea = ar.codArea
 
 select * from Movimiento
