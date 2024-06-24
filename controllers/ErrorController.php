9fb5a490bacc6451e354ecf9a67e7ee8fa708e1b
Miguel Vega Perez <aries_242015@hotmail.com>
2024-06-24 09:40:17 -0500

    fix proteccion de rutas y pagina 404

--- controllers/ErrorController.php
+++ controllers/ErrorController.php
@@ -2,6 +2,6 @@
 
 class ErrorController{
     public function index(){
-        echo '<h1>La pagina que buscas no existe</h1>';
+        require_once "views/layouts/error.php";
     }
 }
---
+++ views/layouts/Error.php
@@ -0,0 +1,4 @@
+<div class="containerError">
+    <h3>La página que buscas no existe :(</h3>
+    <p>Error 404</p>
+</div>
