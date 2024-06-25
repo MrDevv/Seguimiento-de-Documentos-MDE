7a7613fab902b0bdb86e972a6bf6f6dbee4240bd
Rodrigo <estradaleonlarri4@gmail.com>
2024-06-25 09:35:58 -0500

    Actualiza interfaz de registro de admnistrados

--- views/administrado/registro.php
+++ views/administrado/registro.php
@@ -1,4 +1,57 @@
-<?php
-echo ($_SESSION["optionActive"]);
-?>
-<h1>Registro administrado</h1>
+<div class="container-registro-administrado">
+    <h2>REGISTRO ADMINISTRADO</h2>
+        <form>
+            <div class="section">
+                <h3>Datos Generales</h3>
+                <div class="form-group">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" value="Rodrigo">
+                </div>
+
+                <div class="form-group">
+                    <label for="area">Área:</label>
+                        <select id="area" name="area">
+                            <option value="">Seleccione</option>
+                            <option value="">Tramite documentario</option>
+                            <option value="">GAT</option>
+                            <option value="">Mesa de partes</option>
+                                <!-- Agrega más opciones según sea necesario -->
+                        </select>
+                </div>
+                
+                <div class="form-group">
+                    <label for="apellidos">Apellidos:</label>
+                    <input type="text" id="apellidos" name="apellidos" value="Estrada León">
+                </div>
+
+                <div class="form-group">
+                    <label for="estado">Estado:</label>
+                        <select id="estado" name="estado">
+                            <option value="habilitado">Seleccione</option>
+                            <option value="habilitado">Proceso</option>
+                                <!-- Agrega más opciones según sea necesario -->
+                        </select>
+                </div>
+                <div class="form-group">
+                    <label for="telefono">Teléfono:</label>
+                    <input type="text" id="telefono" name="telefono">
+                </div>
+            </div>
+
+            <div class="section">
+                <h3>Datos de Usuario</h3>
+                <div class="form-group">
+                    <label for="usuario">Usuario:</label>
+                    <input type="text" id="usuario" name="usuario" value="restradal" disabled>
+                </div>
+                <div class="form-group">
+                    <label for="contrasena">Contraseña:</label>
+                    <input type="password" id="contrasena" name="contrasena" class="user-input">
+                </div>
+            </div>
+
+            <div class="button-container">
+                <button type="submit">Registrar</button>
+            </div>
+        </form>
+    </div>
