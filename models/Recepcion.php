a4aa409629be7cd03612bfc5a6326afb6aff9207
Miguel Vega Perez <aries_242015@hotmail.com>
2024-07-01 13:08:39 -0500

    agrega clase de Recepcion

--- models/Recepcion.php
+++ models/Recepcion.php
@@ -40,6 +40,28 @@ class Recepcion{
     }
 
     public function registrarRecepcion(){
-        var_dump($this->codRecepcion);
+        $nroDocumento = isset($_POST['nroDocumento']) ? trim($_POST['nroDocumento']) : false;
+        $asunto = isset($_POST['asunto']) ? trim($_POST['asunto']) : false;
+        $folios = isset($_POST['folios']) ? trim($_POST['folios']) : false;
+        $tipoDocumento = isset($_POST['tipoDocumento']) ? trim($_POST['tipoDocumento']) : false;
+        $fechaRegistro = $this->obtenerFechaActual();
+
+        if ($nroDocumento && $asunto && $folios && $tipoDocumento && $fechaRegistro){
+            $documentoObj = new Documento();
+            $documentoObj->setNroDocumento($nroDocumento);
+            $documentoObj->setAsunto($asunto);
+            $documentoObj->setFolios($folios);
+            $documentoObj->setTipoDocumento($tipoDocumento);
+            $documentoObj->setFechaRegistro($fechaRegistro);
+
+//                var_dump($documentoObj);
+//                $response [status, message, info]
+            $response = $documentoObj->guardarNuevoDocumento();
+            var_dump($response);
+            //$_SESSION['response'] = $response;
+            //require_once "views/modals/alerta.php";
+        }
+
     }
+
 }
