d85f9f65b68a818d702d5eae097d1608affdb812
Rodrigo <estradaleonlarri4@gmail.com>
2024-07-01 13:07:44 -0500

    agrega vista de editar administrado

--- views/administrado/editarAdministrado.php
+++ views/administrado/editarAdministrado.php
@@ -1,20 +1,31 @@
+<?php
+require_once('models/Estado.php');
+$estado = new Estado();
+$estados= $estado->listarEstadosHabilitadoInhabilitado();
+?>
 <div class="container-registro-administrado">
-    <h2>REGISTRO ADMINISTRADO</h2>
+    <h2>Actualizar Area</h2>
         <form>
             <div class="section">
                 <h3>Datos Generales</h3>
                 <div class="form-group">
                     <label for="nombre">Nombre:</label>
-                    <input type="text" id="nombre" name="nombre" value="Rodrigo"><br>
+                    <input 
+                    type="text" id="nombre" 
+                    name="nombre" value="<?=$response [0]['nombres']?>"><br>
                 </div>
 
                 <div class="form-group">
                     <label for="area">Área:</label>
                         <select id="area" name="area">
-                            <option value="">Seleccione</option>
-                            <option value="">Tramite documentario</option>
-                            <option value="">GAT</option>
-                            <option value="">Mesa de partes</option>
+                        <?php foreach ($areas as $result):?>
+                            <option 
+                                value="<?=$result['codArea']?>"
+                                <?=$result['codArea']==$response[0]['codArea']?"selected":''?>
+                            >
+                            <?=$result['descripcion']?>
+                            </option>
+                            <?php endforeach; ?>
                                 <!-- Agrega más opciones según sea necesario -->
                         </select><br>
                 </div>
@@ -26,15 +37,21 @@
 
                 <div class="form-group">
                     <label for="estado">Estado:</label>
-                        <select id="estado" name="estado">
-                            <option value="habilitado">Seleccione</option>
-                            <option value="habilitado">Proceso</option>
-                                <!-- Agrega más opciones según sea necesario -->
-                        </select><br>
+                    <select  id="estado" name="estado">
+                        <?php foreach ($estados as $result):?>
+                        <option 
+                        value="<?=$result['codEstado']?>"
+                        <?=$result['codEstado']==$response[0]['codEstado']?"selected":''?>
+                        >
+                        <?=$result['descripcion']?>
+                    </option>
+                        
+                        <?php endforeach; ?>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label for="telefono">Teléfono:</label>
-                    <input type="text" id="telefono" name="telefono"><br>
+                    <input type="text" id="telefono" name="telefono" value="<?=$response [0]['telefono']?>"><br>
                 </div>
             </div>
 
