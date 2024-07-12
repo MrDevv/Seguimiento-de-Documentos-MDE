cd571195be5387a2e5d656d0032c63c090d70d37
Miguel Vega Perez <aries_242015@hotmail.com>
2024-07-12 10:38:17 -0500

    conexion a postgreSQL

--- config/DataBase.php
+++ config/DataBase.php
@@ -1,13 +1,15 @@
 <?php
 
 class DataBase{
-    private static $serverName = "localhost";
+    private static $serverName = "dpg-cq8knjd6l47c73cvt5og-a";
+    private static $port = '5432';
     private static $database = "Sistema_Seguimiento_Documentos";
     private static $username = "sa";
-    private static $password = "123456";
+    private static $password = "UH4PXMCBRau2Uv87u7dX7YHjUDdByJFe";
     public static function connect(){
         try {
-            $conn = new PDO("sqlsrv:server=" . self::$serverName . ";Database=" . self::$database, self::$username, self::$password);
+            //$conn = new PDO("sqlsrv:server=" . self::$serverName . ";port=". self::$port . ";Database=" . self::$database, self::$username, self::$password);
+            $conn = new PDO("postgresql://sa:UH4PXMCBRau2Uv87u7dX7YHjUDdByJFe@dpg-cq8knjd6l47c73cvt5og-a.oregon-postgres.render.com/sistema_seguimiento_documentos");
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }catch(PDOException $e){
