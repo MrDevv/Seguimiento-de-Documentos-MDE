02e3b779e7890758b76a17a966cb56c416ceef8b
Miguel Vega Perez <aries_242015@hotmail.com>
2024-07-17 20:22:16 -0500

    agrega estilos filtrado

--- views/reportes/documentosPorArea.php
+++ views/reportes/documentosPorArea.php
@@ -6,20 +6,24 @@
             <p>Documentos por Area</p>
         </div>
         <form action="<?= base_url ?>reportes/docAreas" method="get">
-            <div class="fecha_busqueda">
+            <div class="containerFiltrado">
                 <div>
-                    Número de Documento:
                     <div class="busqueda">
                         <svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M24.25 22.5002L18.8213 17.4334M18.8213 17.4334C19.7499 16.5667 20.4865 15.5378 20.989 14.4054C21.4916 13.273 21.7503 12.0593 21.7503 10.8336C21.7503 9.60787 21.4916 8.39417 20.989 7.26177C20.4865 6.12937 19.7499 5.10044 18.8213 4.23374C17.8927 3.36704 16.7902 2.67953 15.5769 2.21048C14.3637 1.74142 13.0633 1.5 11.75 1.5C10.4368 1.5 9.13637 1.74142 7.92308 2.21048C6.70979 2.67953 5.60737 3.36704 4.67876 4.23374C2.80335 5.98413 1.74976 8.35816 1.74976 10.8336C1.74976 13.309 2.80335 15.683 4.67876 17.4334C6.55418 19.1838 9.09778 20.1671 11.75 20.1671C14.4022 20.1671 16.9459 19.1838 18.8213 17.4334Z" stroke="#0C7260" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>-->
                         </svg>
-                        <input type="text" name="numDocumento" value="<?= isset($_GET['numDocumento']) ? $_GET['numDocumento'] : ''?>" placeholder="Número de Documento">
+                        <input
+                                type="text"
+                                name="numDocumento"
+                                value="<?= isset($_GET['numDocumento']) ? $_GET['numDocumento'] : ''?>"
+                                autocomplete="off"
+                                placeholder="Número de Documento">
                     </div>
                 </div>
                 <?php if ($_SESSION['user']['rol'] == 'administrador'): ?>
                     <div>
-                        <label>Area</label>
-                        <select class="containerRegistroArea" name="area">
+                        <label>Área:</label>
+                        <select class="selectFiltroArea" name="area">
                             <option value=0 <?= $codArea == null ? 'selected' : '' ?>>Todas</option>
                             <?php foreach ($areas as $result):?>
                                 <option
@@ -32,8 +36,8 @@
                         </select>
                     </div>
                 <?php endif; ?>
+                <input type="submit" value="Filtrar">
             </div>
-            <input type="submit" value="Filtrar">
         </form>
 
     </div>
