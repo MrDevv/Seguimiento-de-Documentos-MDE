2f9c35c3eb27ac504c95575ae04237a0911f71cb
Miguel Vega Perez <aries_242015@hotmail.com>
2024-07-21 23:10:23 -0500

    modifica estilos de los modales de registrar y editar usuario

--- views/usuario/editarUsuario.php
+++ views/usuario/editarUsuario.php
@@ -1,5 +1,5 @@
 <div id="modalEditarUsuario" class="modalArea modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Editar Usuario</h5>
--- views/usuario/registro.php
+++ views/usuario/registro.php
@@ -1,5 +1,5 @@
 <div id="modalRegistrarUsuario" class="modalArea modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Registrar Nuevo Usuario</h5>
