17323e1d0bdbcde295797bfa5117f2b7cdeec6a8
Miguel Vega Perez <aries_242015@hotmail.com>
2024-08-11 23:13:23 -0500

    fix elimina tildes del usuario

--- ajax/listarUsuario.js
+++ ajax/listarUsuario.js
@@ -702,14 +702,22 @@ $(document).ready(function() {
     const apellidoInput = $('#apellidosEditar');
     const usuarioInput = $('#usuarioEditar');
 
+    function quitarTildes(texto) {
+        const acentos = {
+            'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+            'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U'
+        };
+        return texto.split('').map(letra => acentos[letra] || letra).join('');
+    }
+
     function generarUsuario() {
         const nombre = nombreInput.val().trim();
         const apellidos = apellidoInput.val().trim().split(' ');
 
         if (nombre && apellidos.length >= 2) {
             const primerNombreLetra = nombre.charAt(0).toLowerCase();
-            const primerApellido = apellidos[0].toLowerCase();
-            const segundoApellidoLetra = apellidos[1].charAt(0).toLowerCase();
+            const primerApellido = quitarTildes(apellidos[0].toLowerCase());
+            const segundoApellidoLetra = quitarTildes(apellidos[1].charAt(0).toLowerCase());
 
             usuarioInput.val(primerNombreLetra + primerApellido + segundoApellidoLetra);
         } else {
--- helpers/generarUserName.js
+++ helpers/generarUserName.js
@@ -4,14 +4,22 @@ $(document).ready(function() {
     const apellidoInput = $('#apellidosNuevo');
     const usuarioInput = $('#usuarioNuevo');
 
+    function quitarTildes(texto) {
+        const acentos = {
+            'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u',
+            'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U'
+        };
+        return texto.split('').map(letra => acentos[letra] || letra).join('');
+    }
+
     function generarUsuario() {
         const nombre = nombreInput.val().trim();
         const apellidos = apellidoInput.val().trim().split(' ');
 
         if (nombre && apellidos.length >= 2) {
             const primerNombreLetra = nombre.charAt(0).toLowerCase();
-            const primerApellido = apellidos[0].toLowerCase();
-            const segundoApellidoLetra = apellidos[1].charAt(0).toLowerCase();
+            const primerApellido = quitarTildes(apellidos[0].toLowerCase());
+            const segundoApellidoLetra = quitarTildes(apellidos[1].charAt(0).toLowerCase());
 
             usuarioInput.val(primerNombreLetra + primerApellido + segundoApellidoLetra);
         } else {
@@ -22,4 +30,3 @@ $(document).ready(function() {
     nombreInput.on('input', generarUsuario);
     apellidoInput.on('input', generarUsuario);
 });
-
